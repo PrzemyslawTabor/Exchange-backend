a91a35a0a5413ebb0d50b6eed2badc8c6cac6140
Przemysław Tabor <ptabor@future-processing.com>
2023-08-29 22:00:19 +0200

    CR changes

--- app.js
+++ app.js
@@ -25,35 +25,45 @@ app.post('/wallet/create', async (req, res) => {
   res.status(201).json({ message: 'Wallet created.', wallet: newWallet });
 });
 
-app.post('/wallet/update', async (req, res) => {
+app.post('/wallet/buy', async (req, res) => {
   const walletData = req.body;
   const existingWallet = await getStoredWallet(walletData.data.userId);
   const wallets = await getAllStoredWallets();
 
   let keys = Object.keys(existingWallet);
   keys.map(key => {
-    if (walletData.data.option === "buy") {
-      if (key === walletData.data.values.currencyCode) {
-        existingWallet[key] = existingWallet[key] + parseInt(walletData.data.values.units);
-      }
-
-      if (key === 'availableMoney') {
-        existingWallet[key] = existingWallet[key] - parseFloat(walletData.data.values.totalValue);
-      }
+    if (key === walletData.data.values.currencyCode) {
+      return existingWallet[key] = existingWallet[key] + walletData.data.values.units;
     }
 
-    if (walletData.data.option === "sell") {
-      if (key === walletData.data.values.currencyCode) {
-        existingWallet[key] = existingWallet[key] - parseInt(walletData.data.values.units);
-      }
-
-      if (key === 'availableMoney') {
-        existingWallet[key] = existingWallet[key] + parseFloat(walletData.data.values.totalValue);
-      }
+    if (key === 'availableMoney') {
+      return existingWallet[key] = existingWallet[key] - walletData.data.values.totalValue;
     }
   })
 
-  wallets[wallets.findIndex(x=>x.userId == walletData.data.userId)] = existingWallet;
+  wallets[walletData.data.userId] = existingWallet;
+  await storeWallet(wallets);
+
+  res.status(201).json({ message: 'Wallet updated.', wallet: existingWallet });
+});
+
+app.post('/wallet/sell', async (req, res) => {
+  const walletData = req.body;
+  const existingWallet = await getStoredWallet(walletData.data.userId);
+  const wallets = await getAllStoredWallets();
+
+  let keys = Object.keys(existingWallet);
+  keys.map(key => {
+    if (key === walletData.data.values.currencyCode) {
+      return existingWallet[key] = existingWallet[key] - walletData.data.values.units;
+    }
+
+    if (key === 'availableMoney') {
+      return existingWallet[key] = existingWallet[key] + walletData.data.values.totalValue;
+    }
+  })
+
+  wallets[walletData.data.userId] = existingWallet;
   await storeWallet(wallets);
 
   res.status(201).json({ message: 'Wallet updated.', wallet: existingWallet });
--- data/wallet.js
+++ data/wallet.js
@@ -2,7 +2,7 @@ const fs = require('node:fs/promises');
 
 async function getStoredWallet(userId) {
   const storedWallets = await getAllStoredWallets();
-  let wallet = storedWallets.find((wallet) => wallet.userId == userId);
+  const wallet = storedWallets[userId];
 
   if (wallet === undefined) {
     return null;
@@ -14,23 +14,23 @@ async function getStoredWallet(userId) {
 async function getAllStoredWallets() {
   const rawFileContent = await fs.readFile('wallet.json', { encoding: 'utf-8' });
   const data = JSON.parse(rawFileContent);
-  const storedWallets = data.wallet ?? [];
+  const storedWallets = data.wallets ?? [];
 
   return storedWallets;
 }
 
-function storeWallet(wallet) {
-  return fs.writeFile('wallet.json', JSON.stringify({ wallet: wallet || [] }));
+function storeWallet(wallets) {
+  return fs.writeFile('wallet.json', JSON.stringify({ wallets: wallets || [] }));
 }
 
 async function createNewWallet(values, userId) {
-  const newWallet = Object.entries(values).reduce((obj, [key, value]) => (obj[key] = parseFloat(value), obj), {});
-  newWallet.userId = userId;
-
+  const newWallet = { userId }; Object.entries(values).forEach(([key, value]) => (newWallet[key] = value));
   const wallets = await getAllStoredWallets();
-  await storeWallet([newWallet, ...wallets]);
 
-  return newWallet;
+  wallets[userId] = newWallet;
+  await storeWallet(wallets);
+  
+  return wallets[userId];
 }
 
 
